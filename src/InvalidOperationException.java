027eae0124ee93f7776d0c2ea857d8650f50bf12
Israel Hill <idh@case.edu>
2015-11-20 04:31:14 -0500

    added some innards to Exception class

--- src/InvalidOperationException.java
+++ src/InvalidOperationException.java
@@ -1,2 +1,11 @@
 public class InvalidOperationException extends Exception {
+    private String message;
+
+    public InvalidOperationException(String message) {
+        this.message = message;
+    }
+
+    public String toString() {
+        return message;
+    }
 }
